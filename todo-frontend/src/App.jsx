1e53d4a2e414e95bf595fe6729c48ba093dce907
cristina <neacsucristinamihaela@yahoo.com>
2025-07-26 18:17:36 +0200

    add routing and initial features: notes, layout, todo + downgrade react + remove boilerplate code

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Layout from "./features/layout/Layout.jsx";
+import Notes from "./features/notes/Notes.jsx";
+import ToDo from "./features/todo/ToDo.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+          <BrowserRouter>
+              <Routes>
+                  <Route element={<Layout/>}>
+                      <Route path={"/"} element={<Navigate to={"todo"} replace/>} />
+
+                      <Route path="todo" element={<ToDo/>} />
+                      <Route path="notes" element={<Notes/>} />
+                  </Route>
+              </Routes>
+          </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
---
+++ todo-frontend/src/features/todo/ToDo.jsx
@@ -0,0 +1,9 @@
+function ToDo() {
+    return (
+        <div>
+            Toodoopoopoo
+        </div>
+    );
+}
+
+export default ToDo;
