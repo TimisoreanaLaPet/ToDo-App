45e1c2f67ceb811b3d6926229b39ecb5fafb4484
cristina <neacsucristinamihaela@yahoo.com>
2025-07-28 10:34:32 +0200

    initial commit for To Do page

--- todo-frontend/src/features/todo/ToDo.jsx
+++ todo-frontend/src/features/todo/ToDo.jsx
@@ -1,9 +1,18 @@
-import {Stack, Typography} from "@mui/material";
+import {Checkbox, FormControlLabel, FormGroup, InputBase, Stack, TextField, Typography} from "@mui/material";
 
 function ToDo() {
     return (
-        <Stack>
+        <Stack direction={'column'}>
             <Typography variant={"h4"} fontWeight={600} color={'#505050'}>To Do List</Typography>
+            <Stack direction={'row'}>
+                <Stack direction={'column'}>
+                    <Typography variant={'h5'} fontWeight={500} color={'#505050'}>Monday</Typography>
+                    <Stack direction={'row'}>
+                        <Checkbox color={'#505050'}/>
+                        <InputBase sx={{ ml: 1, flex: 1 }}/>
+                    </Stack>
+                </Stack>
+            </Stack>
         </Stack>
     );
 }
